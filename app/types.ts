deaf79d2275438f52d95966674158f8b5acfd6db
Samuel <samuel05.hb@gmail.com>
2026-04-11 11:39:06 -0400

    ui/Created Homepage.

---
+++ app/types.ts
@@ -0,0 +1,8 @@
+export interface FeatureCardProps {
+    page: string;
+    icon: React.ReactNode;
+    title: string;
+    description: string;
+    bg_color?: string;
+    border_color?: string;
+}
